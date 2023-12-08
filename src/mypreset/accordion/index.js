c3fd2f6249519d9d59432f07d2bcfb0445fa8e0d
Cagatay Civici <cagatay.civici@gmail.com>
2023-12-08 22:09:18 +0300

    Update focus

--- src/mypreset/accordion/index.js
+++ src/mypreset/accordion/index.js
@@ -4,10 +4,10 @@ export default {
         headerAction: ({ context }) => ({
             class: [
                 "flex flex-row-reverse justify-between items-center cursor-pointer relative no-underline select-none", // Alignments
-                "p-5 transition-color duration-300 ease-in-out font-bold", // Padding and transition
+                "p-5 transition-all duration-300 ease-in-out font-bold mb-1", // Padding and transition
                 "border-b border-surface-200 bg-white text-surface-700 dark:border-surface-700 dark:bg-transparent dark:text-surface-400", // Borders and colors
                 "hover:bg-surface-50 hover:text-surface-800 dark:hover:bg-surface-800 dark:hover:text-surface-300", // Hover
-                "focus:outline-none focus:outline-offset-0 focus-visible:shadow-[inset_0_0_0_0.2rem_rgba(191,219,254,1)]", // Focus
+                "focus:outline-0 focus-visible:ring-2 focus-visible:ring-surface-200", // Focus
             ],
         }),
         headerTitle: "leading-none",
